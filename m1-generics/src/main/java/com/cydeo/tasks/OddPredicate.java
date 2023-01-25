371b7866618694a5842eed35a53fe244abfd3661
HadiHarmandar <hadiharmandar1@gmail.com>
2023-01-24 19:19:49 -0600

    Tasks

--- m1-generics/src/main/java/com/cydeo/tasks/OddPredicate.java
+++ m1-generics/src/main/java/com/cydeo/tasks/OddPredicate.java
@@ -1,7 +1,5 @@
 package com.cydeo.tasks;
 
-import tasks.task2.UnaryPredicate;
-
 public class OddPredicate implements UnaryPredicate<Integer> {
     @Override
     public boolean test(Integer obj) {
