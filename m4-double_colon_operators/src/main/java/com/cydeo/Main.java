bd0bd59f7c3518a06cd0b608f5ccd5135e5bf6fd
HadiHarmandar <hadiharmandar1@gmail.com>
2023-01-30 23:42:35 -0600

    Updates

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,6 +1,7 @@
 package com.cydeo;
 
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -31,6 +32,12 @@ public class Main {
         Function<Integer, Double> b = new MyClass()::method;
         BiFunction<MyClass, Integer, Double> b1 = MyClass::method;
         System.out.println(b1.apply(new MyClass(), 5));
+
+        Consumer<Integer> display = i -> System.out.println(i);
+        Consumer<Integer> display2 = System.out::println;
+
+
+
 
 
 
