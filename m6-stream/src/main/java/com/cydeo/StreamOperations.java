08617ea5161e81c7b8425921f095d0e6fbdfcafb
HadiHarmandar <hadiharmandar1@gmail.com>
2023-02-01 00:50:16 -0600

    Dish Task

--- m6-stream/src/main/java/com/cydeo/StreamOperations.java
+++ m6-stream/src/main/java/com/cydeo/StreamOperations.java
@@ -8,7 +8,7 @@ public class StreamOperations {
 
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 4);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
         //list.forEach(System.out::println);
 
         // FILTER
@@ -32,6 +32,32 @@ public class StreamOperations {
                 .limit(1)
                 .forEach(System.out::println);
 
+        // SKIP
+        System.out.println("Skip");
+        list.stream()
+                .filter(i -> i % 2 == 0)
+                .skip(1)
+                .forEach(System.out::println);
+
+        // MAP
+        System.out.println("Map");
+        list.stream()
+                .map(number -> number * 2)
+                .filter(i -> i % 3 == 0)
+                .forEach(System.out::println);
+
+        // TASK
+        System.out.println("Task");
+        List<String> words = Arrays.asList("JAVA", "APPLE", "HONDA", "DEVELOPER");
+        words.stream()
+                .map(w -> w.length())
+                .forEach(System.out::println);
+
+        // WITHOUT STREAM
+        System.out.println("Without Stream");
+        for (String word : words) {
+            System.out.println(word.length());
+        }
 
 
     }
