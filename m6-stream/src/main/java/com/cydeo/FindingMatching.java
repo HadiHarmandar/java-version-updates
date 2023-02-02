a9947fc27e0fd7c713257ef813c7954073d693dd
HadiHarmandar <hadiharmandar1@gmail.com>
2023-02-01 23:09:57 -0600

    Finding Matching

--- m6-stream/src/main/java/com/cydeo/FindingMatching.java
+++ m6-stream/src/main/java/com/cydeo/FindingMatching.java
@@ -3,7 +3,11 @@ package com.cydeo;
 import com.cydeo.task.Dish;
 import com.cydeo.task.DishData;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 public class FindingMatching {
 
@@ -29,6 +33,41 @@ public class FindingMatching {
         System.out.println("FIND MATCH");
         Optional<Dish> dish = DishData.getAll().stream().filter(Dish::isVegetarian).findAny();
         System.out.println(dish.get());
+
+        // FIND FIRST
+        System.out.println("FIND FIRST");
+        Optional<Dish> dish2 = DishData.getAll().stream().filter(Dish::isVegetarian).findFirst();
+        System.out.println(dish2.get());
+
+        // PARALLEL STREAMS (Async)
+        System.out.println(IntStream.range(0, 100).parallel().findAny());
+        System.out.println(IntStream.range(0, 100).parallel().findFirst());
+
+        List<String> list1 = Arrays.asList("Jhonny", "David", "Jack", "Duke", "Jill", "Dany", "Julia", "Jenish", "Divya");
+        List<String> list2 = Arrays.asList("Jhonny", "David", "Jack", "Duke", "Jill", "Dany", "Julia", "Jenish", "Divya");
+
+        Optional<String> findFirst = list1.parallelStream().filter(s -> s.startsWith("D")).findFirst();
+        Optional<String> findAny = list2.parallelStream().filter(s -> s.startsWith("J")).findAny();
+
+        System.out.println(findFirst.get()); // parallelStream() ==> Async
+        System.out.println(findAny.get());
+
+        System.out.println("Without parallel");
+        Optional<String> findFirst1 = list1.stream().filter(s -> s.startsWith("D")).findFirst();
+        Optional<String> findAny1 = list2.stream().filter(s -> s.startsWith("J")).findAny();
+
+        System.out.println(findFirst1.get());
+        System.out.println(findAny1.get());
+
+        // MIN
+        System.out.println("MIN");
+        Optional<Dish> dMin = DishData.getAll().stream().min(Comparator.comparing(Dish::getCalories));
+        System.out.println(dMin.get());
+
+        // MAX
+        System.out.println("MAX");
+        Optional<Dish> dMax = DishData.getAll().stream().max(Comparator.comparing(Dish::getCalories));
+        System.out.println(dMax.get());
 
     }
 }
