c4b5003b5b8f22790af76e1bbb8d8b47a0082d7b
HadiHarmandar <hadiharmandar1@gmail.com>
2023-02-11 11:56:09 -0600

    Java12

--- m8-new-version-updates/src/main/java/com/cydeo/java12/GuestTask.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java12/GuestTask.java
@@ -13,9 +13,16 @@ public class GuestTask {
                                 new Guest("Marco", true, 3),
                                 new Guest("David", false, 2),
                                 new Guest("Roger", true, 6))
-                        .collect(Collectors.teeing(Collectors.filtering(Guest::isParticipating,
+                        .collect(Collectors.teeing(
+                                // first collector, we select only confirmed the participation
+                                Collectors.filtering(Guest::isParticipating,
+                                        // we want to collect only the first name in a list
                                         Collectors.mapping(Guest::getName, Collectors.toList())),
-                                Collectors.filtering(Guest::isParticipating, Collectors.summingInt(Guest::getParticipantsNumber)), EventParticipation::new));
+                                // second collector, we want the total number of participation
+                                Collectors.filtering(Guest::isParticipating, Collectors.summingInt(Guest::getParticipantsNumber)),
+                                // we merge the collectors on a new Object,
+                                // the values ar implicitly passed
+                                EventParticipation::new));
 
         System.out.println(result);
 
