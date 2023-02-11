c464137ec33ed047b3c42d821b403a7db01c895f
HadiHarmandar <hadiharmandar1@gmail.com>
2023-02-11 12:42:52 -0600

    Java14

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,14 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) {
+
+    // private final ==> all fields
+    // ALL ARG CONST
+    // GETTER
+    // TO STRING
+    // EQUALS
+    // HASH CODE
+    public static void someMethod(int a) {
+        System.out.println(a);
+    }
+}
